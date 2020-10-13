483825f1868bcfac1465823a7eeb6c4b4bd4ab3a
Zool-ee <z.marki85@gmail.com>
2020-10-13 12:23:10 +0200

    Feltölt

---
+++ TDD/Szoftvertech_beadando_esze/Szoftvertech_beadando_esze/TDDEsze.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szoftvertech_beadando_esze
+{
+    public class TDDesze
+
+    {
+        public static string GetValue(int input)
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ TDD/Szoftvertech_beadando_esze/Szoftvertech_beadando_esze_teszt/TDDeszeTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Szoftvertech_beadando_esze;
+
+namespace Szoftvertech_beadando_esze_teszt
+{
+    [TestFixture]
+    public class TDDeszeTest
+    {
+        [Test]
+        public void esze_when1_return1()
+        {
+            int input = 1;
+            string output = TDDesze.GetValue(input);
+
+
+        }
+    }
+}
