bb969a13d1f5f914e000d97d1b50af217536f28e
Zool-ee <z.marki85@gmail.com>
2020-10-23 18:03:49 +0200

    elsö teszt

--- TDD/Szoftvertech_beadando_esze/Szoftvertech_beadando_esze_teszt/TDDeszeTest.cs
+++ TDD/Szoftvertech_beadando_esze/Szoftvertech_beadando_esze_teszt/TDDeszeTest.cs
@@ -12,7 +12,7 @@ namespace Szoftvertech_beadando_esze_teszt
     public class TDDeszeTest
     {
         [Test]
-        public void esze_when1_return1()
+        public void nagyobb()
         {
             //arrage
             int input = 1;
@@ -21,23 +21,10 @@ namespace Szoftvertech_beadando_esze_teszt
             //assert
             if (output > 0)
                 Assert.Pass();
-            else
-                Assert.Fail();
-        }
-        [Test]
-        public void esze_when2_return2()
-        {
-            //arrage
-            int input = 95;
-            //act
-            int output = TDDesze.GetValue(input);
-            //assert
-            if (output <= 90)
-            {
-                Assert.Pass();
-            }
-            else
-                Assert.Fail();
+            Assert.Fail();
+         
+
         }
     }
 }
+       
